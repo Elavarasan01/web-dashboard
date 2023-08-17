38cb949717c63a68a4c3be2734f1004a193b1c0a
Senthilkumar Elavarasan <elavarasan.senthil@peopletech.com>
2023-08-17 21:29:55 +0530

    pushed latest code

--- src/container.js
+++ src/container.js
@@ -4,50 +4,64 @@ export const Middle = () => {
       <div className="left-div">
         <h1 className="middle-header">Groups</h1>
         <div className="mid-divs">
-        <h2 style={{color:"#6F747E"}}>Search Groups..</h2>
-        <input
-          type="search"
-          placeholder="Search Groups.."
-          className="search-middle"
-        />
-        <button className="search-button">Search</button>
+          <h2 style={{ color: "#6F747E" }}>Search Groups..</h2>
+          <input
+            type="search"
+            placeholder="Search Groups.."
+            className="search-middle"
+          />
+          <button className="search-button">Search</button>
         </div>
         <div className="mid-divs">
-            <p className="mid-para" style={{color:"#FAB19D"}}>All Groups<span className="batch">5</span></p>
-            <p className="mid-para">My Groups<span className="batch">3</span></p>
-            <hr style={{width:"100%"}}/>
-            <select className="mid-select">
-                <option>Last Active</option>
-                <option>1hr ago</option>
-                <option>1day ago</option>
-                <option>1week ago</option>
-            </select>
+          <p className="mid-para" style={{ color: "#FAB19D" }}>All Groups<span className="batch">5</span></p>
+          <p className="mid-para">My Groups<span className="batch">3</span></p>
+          <hr style={{ width: "100%" }} />
+          <select className="mid-select">
+            <option>Last Active</option>
+            <option>1hr ago</option>
+            <option>1day ago</option>
+            <option>1week ago</option>
+          </select>
         </div>
         <div className="card-section">
-        <div class="card">
-  <img src="img_avatar2.png" alt="Avatar" style={{width:"100%"}} />
-  <div class="container">
-    <h4><b>Jane Doe</b></h4> 
-    <p>Interior Designer</p> 
-  </div>
-</div>
+          <div class="card">
+            <img src="img_avatar2.png" alt="Avatar" style={{ width: "100%",height:"130px" }} />
+            <div class="container">
+              <h4><b>Jane Doe</b></h4>
+              <p>Interior Designer</p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img_avatar2.png" alt="Avatar" style={{ width: "100%",height:"120px" }} />
+            <div class="container">
+              <h4><b>Jane Doe</b></h4>
+              <p>Interior Designer</p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img_avatar2.png" alt="Avatar" style={{ width: "100%",height:"120px" }} />
+            <div class="container">
+              <h4><b>Jane Doe</b></h4>
+              <p>Interior Designer</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="right-div">
         <div className="right-top">
-            <h2>Active Members</h2>
-            <img src="img_avatar.png" alt="face" className="face-batch"/>
-            <img src="img_avatar.png" alt="face" className="face-batch"/>
-            <img src="img_avatar.png" alt="face" className="face-batch"/>
-            <img src="img_avatar.png" alt="face" className="face-batch"/>
-            <p>view all</p>
+          <h2>Active Members</h2>
+          <img src="img_avatar.png" alt="face" className="face-batch" />
+          <img src="img_avatar.png" alt="face" className="face-batch" />
+          <img src="img_avatar.png" alt="face" className="face-batch" />
+          <img src="img_avatar.png" alt="face" className="face-batch" />
+          <p>view all</p>
         </div>
         <div className="right-down">
-            <h2 style={{paddingTop:"30px"}}>Recent Topics</h2>
-            <p><img src="img_avatar.png" alt="face" className="face-batch"/></p>
-            <p><img src="img_avatar.png" alt="face" className="face-batch"/></p>
-            <p><img src="img_avatar.png" alt="face" className="face-batch"/></p>
-            <p><img src="img_avatar.png" alt="face" className="face-batch"/></p>
+          <h2 style={{ paddingTop: "30px" }}>Recent Topics</h2>
+          <p><img src="img_avatar.png" alt="face" className="face-batch" /></p>
+          <p><img src="img_avatar.png" alt="face" className="face-batch" /></p>
+          <p><img src="img_avatar.png" alt="face" className="face-batch" /></p>
+          <p><img src="img_avatar.png" alt="face" className="face-batch" /></p>
         </div>
       </div>
     </div>
